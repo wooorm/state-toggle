cc6df84fea55b4a0c66d7186aff7ded84194ff86
Titus Wormer <tituswormer@gmail.com>
2018-04-21 11:36:10 +0200

    Update dev-dependencies

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var test = require('tape');
-var toggle = require('./');
+var toggle = require('.');
 
 test('toggle()', function (t) {
   t.test('no context object', function (st) {
