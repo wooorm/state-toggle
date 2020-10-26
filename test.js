f20d6b5c712bf0e33b8c5ad19c5c92f84e0aaac6
Titus Wormer <tituswormer@gmail.com>
2020-10-26 20:29:25 +0100

    Refactor tests

--- test.js
+++ test.js
@@ -4,68 +4,68 @@ var test = require('tape')
 var toggle = require('.')
 
 test('toggle()', function (t) {
-  t.test('no context object', function (st) {
+  t.test('no context object', function (t) {
     var ctx = {on: false}
     var exit
 
     ctx.enter = toggle('on', ctx.on)
 
-    st.equal(ctx.on, false, 'should start off')
+    t.equal(ctx.on, false, 'should start off')
     exit = ctx.enter()
-    st.equal(ctx.on, true, 'should toggle on')
+    t.equal(ctx.on, true, 'should toggle on')
     exit()
-    st.equal(ctx.on, false, 'should revert')
+    t.equal(ctx.on, false, 'should revert')
 
-    st.end()
+    t.end()
   })
 
-  t.test('context object', function (st) {
+  t.test('context object', function (t) {
     var ctx = {on: false}
     var enter = toggle('on', ctx.on, ctx)
     var exit
 
-    st.equal(ctx.on, false, 'should start off')
+    t.equal(ctx.on, false, 'should start off')
     exit = enter()
-    st.equal(ctx.on, true, 'should toggle on')
+    t.equal(ctx.on, true, 'should toggle on')
     exit()
-    st.equal(ctx.on, false, 'should revert')
+    t.equal(ctx.on, false, 'should revert')
 
-    st.end()
+    t.end()
   })
 
-  t.test('initial state', function (st) {
+  t.test('initial state', function (t) {
     var ctx = {on: 1}
     var enter = toggle('on', false, ctx)
     var exit
 
-    st.equal(ctx.on, 1, 'should start on the initial state')
+    t.equal(ctx.on, 1, 'should start on the initial state')
     exit = enter()
-    st.equal(ctx.on, true, 'should toggle on')
+    t.equal(ctx.on, true, 'should toggle on')
     exit()
-    st.equal(ctx.on, 1, 'should revert')
+    t.equal(ctx.on, 1, 'should revert')
 
-    st.end()
+    t.end()
   })
 
-  t.test('multiple state', function (st) {
+  t.test('multiple state', function (t) {
     var ctx = {on: false}
     var enter = toggle('on', ctx.on, ctx)
     var exitA
     var exitB
 
-    st.equal(ctx.on, false, 'should start on the initial state')
+    t.equal(ctx.on, false, 'should start on the initial state')
 
     exitA = enter()
-    st.equal(ctx.on, true, 'should toggle on')
+    t.equal(ctx.on, true, 'should toggle on')
     exitB = enter()
-    st.equal(ctx.on, true, 'should not toggle again')
+    t.equal(ctx.on, true, 'should not toggle again')
 
     exitB()
-    st.equal(ctx.on, true, 'should not revert when nested')
+    t.equal(ctx.on, true, 'should not revert when nested')
     exitA()
-    st.equal(ctx.on, false, 'should revert when not nested')
+    t.equal(ctx.on, false, 'should revert when not nested')
 
-    st.end()
+    t.end()
   })
 
   t.end()
